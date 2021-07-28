f4d3c50ca8ad1a18725dea5ec7485e177423fa02
unknown <mittervan@gmail.com>
2021-07-28 16:32:06 +0700

    Is prime

---
+++ src/IsPrime/Main.java
@@ -0,0 +1,10 @@
+package IsPrime;
+
+public class Main {
+    public static void main(String[] args) {
+        IsPrime1 isPrime1 = new IsPrime1();
+        IsPrime2 isPrime2 = new IsPrime2();
+        isPrime1.start();
+        isPrime2.start();
+    }
+}
--- src/OddEven/Main.java
+++ src/OddEven/Main.java
@@ -3,9 +3,20 @@ package OddEven;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         OddThread oddThread = new OddThread();
-        EvenThread evenThread = new EvenThread();
+        EvenThread evenThread = new EvenThread(){
+            public void run(){
+                for (int i = 0; i < 3; i++) {
+                    System.out.println("sleeping");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                };
+            }
+        };
         oddThread.start();
-        oddThread.join();
+        evenThread.join();
         evenThread.start();
     }
 }
